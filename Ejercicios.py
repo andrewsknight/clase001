3a0cbd27865da78c2b95f00213743ef035c785b4
Andres <pyperan@gmail.com>
2022-01-26 17:19:34 +0100

    actualizo ejercicio

--- Ejercicios.py
+++ Ejercicios.py
@@ -1,6 +1,16 @@
+
+# creo la clase
 class Perro():
+    #siempre en un objeto crear el constructor.
     def __init__(self, nombre, edad, peso):
         self.nombre = nombre
         self.edad = edad
         self.peso = peso
-        
+    
+    def ladrar(self):
+        if self.peso > 3:
+            print("Guau, Guau")
+        else:
+            print("ay, ay")
+    def __str__(self):
+        return "Guau, soy {} y peso {} kg".format( self.nombre, self.peso)
